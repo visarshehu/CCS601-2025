522175daaf5a2afeb4744a96d219d2270f62a5ff
Visar Shehu <visars@inspire11.com>
2025-10-30 13:25:51 +0100

    added point light and orbit controls

--- Java IV/main.js
+++ Java IV/main.js
@@ -4,26 +4,26 @@ import { contain } from 'three/src/extras/TextureUtils.js';
 
 const scene = new THREE.Scene();
 
-// const camera = new THREE.PerspectiveCamera(
-//     75, 
-//     window.innerWidth / window.innerHeight, 
-//     0.1, 
-//     1000
-// )
-const camera = new THREE.OrthographicCamera(
-    window.innerWidth / -200,
-    window.innerWidth / 200,
-    window.innerHeight / 200,
-    window.innerHeight / -200,
-    0.1,
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight, 
+    0.1, 
     1000
 );
+// const camera = new THREE.OrthographicCamera(
+//     window.innerWidth / -200,
+//     window.innerWidth / 200,
+//     window.innerHeight / 200,
+//     window.innerHeight / -200,
+//     0.1,
+//     1000
+// );
 
-camera.position.set(0, 2, 5);
+camera.position.set(0, 2, 3);
 camera.lookAt(0, 0, 0);
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial({ 
+const boxGeometry = new THREE.SphereGeometry(1, 32, 32);
+const boxMaterial = new THREE.MeshStandardMaterial({ 
         color: 0x00ff00
     });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
@@ -31,18 +31,34 @@ boxMesh.position.set(-2, 0, -2);
 
 scene.add(boxMesh);
 
-const box1Material = new THREE.MeshBasicMaterial({ 
+const box1Material = new THREE.MeshPhongMaterial({ 
         color: 0x0000ff
     });
 const box1Mesh = new THREE.Mesh(boxGeometry, box1Material);
 box1Mesh.position.set(2, 0, 0);
 scene.add(box1Mesh);
 
-const light = new THREE.AmbientLight(0xffffff, 1);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
 scene.add(light);
+
+const pointLight = new THREE.PointLight(0xff0000, 0.8, 25  );
+pointLight.position.set(-3, -3, 3);
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(pointLight);
+scene.add(pointLightHelper);
 
 const container = document.getElementById('scene1');
 const renderer = new THREE.WebGLRenderer();
 container.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
