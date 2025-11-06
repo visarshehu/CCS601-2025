b3169e3f2ee8159434e7216a471cccef06e7479f
Visar Shehu <visars@inspire11.com>
2025-11-06 12:18:29 +0100

    Implemented a basic FBX Loader

--- Java V/main.js
+++ Java V/main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(     
@@ -17,11 +19,19 @@ const material = new THREE.MeshStandardMaterial(
 );
 
 const cubeMesh = new THREE.Mesh(geometry, material);
-scene.add(cubeMesh);
+//scene.add(cubeMesh);
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2, 2, 3);
 scene.add(light);
+
+
+const fbxLoader = new FBXLoader();
+
+const object = await fbxLoader.loadAsync('X Bot.fbx');
+object.scale.set(0.01, 0.01, 0.01);
+
+scene.add(object);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -29,7 +39,7 @@ function animate()
 {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    cubeMesh.rotation.x += 0.01;
+    object.rotation.y += 0.01;
 }
 
 animate();
